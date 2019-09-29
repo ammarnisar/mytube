4ec08085f099c2cf2be7a26994e1d0d86f907776
Ammar Nisar <ammarnisar1993@gmail.com>
2019-09-29 18:14:43 +0500

    allowed host added added

--- mytube/settings.py
+++ mytube/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l(sxrn@(mq#u#@q37#8d1)86&s@&=2+g@g%#ml-7dasnx^bwil'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','34.70.180.36']
 
 
 # Application definition
